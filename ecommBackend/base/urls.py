b4490a6fdfeaf2e7ead843ce8f4aadb26a11253b
Rupam <rupam@pop-os.localdomain>
2021-05-04 00:38:29 +0530

    setup of json web token complete

--- ecommBackend/base/urls.py
+++ ecommBackend/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns=[
+     path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('',views.getResponse,name='routes'),
     path('products/',views.getProducts, name='products'),
     path('products/<str:prodId>/',views.getProductById,name="GetProductsById"),
